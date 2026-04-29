aeeffd35c9a373ec4ad9024d2b2f81d57320e8ae
SunniesCloud <rowena.sasi@triosstudent.com>
2026-04-29 18:50:17 -0400

    Typescript

--- REACT_Ch2/calculateTotalPrice.ts
+++ REACT_Ch2/calculateTotalPrice.ts
@@ -1,21 +1,24 @@
 function calculateTotalPrice(
     product: {name: string, unitPrice: number},
     quantity: number,
-     discount: number)
+     discount: number): number
 {
     const priceWithoutDiscount = product.unitPrice * quantity;
     const discountAmount = priceWithoutDiscount * discount;
     return (priceWithoutDiscount + discountAmount);
 }
 
+const total: number = calculateTotalPrice({name: "Soccer Ball", unitPrice: 12.99}, 10, 0.2);
+console.log(total);
+
 function getTotal(
     unitPrice: number,
     quantity: number,
-     discount: number)
+     discount: number): number
 {
     const priceWithoutDiscount = unitPrice * quantity;
     const discountAmount = priceWithoutDiscount * discount;
     return (priceWithoutDiscount - discountAmount);
 }
 
-let total: number = getTotal(500, 1, 0.1)
+let total2: number = getTotal(500, 1, 0.1);
